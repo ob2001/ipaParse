b0a59b76fbdd9f800549c4a48541e19fa9384250
ob2001 <butlero2001@outlook.com>
2023-10-18 22:05:46 -0400

    Allow parsing entire string written in x-sampa into ipa

--- ipaparse.py
+++ ipaparse.py
@@ -3,30 +3,38 @@ import tomli
 with open("x-sampa.toml", mode = 'rb') as fp:
     data = tomli.load(fp)
 
+tokens = data.keys()
+
 def get_ipa_char(code):
     return data[code]["ipa"]
 
-def print_ipa_char(code):
-    print(data[code]["ipa"])
-
 def parse_str(str):
-    pass
-
-def hello_world_by_chars():
-    print("["
-        + get_ipa_char("<F>")
-        + get_ipa_char("\"")
-        + get_ipa_char("h")
-        + get_ipa_char("E")
-        + get_ipa_char("%")
-        + get_ipa_char("l")
-        + get_ipa_char("o")
-        + " "
-        + get_ipa_char("<R>")
-        + get_ipa_char("w")
-        + get_ipa_char("@`")
-        + get_ipa_char("l")
-        + get_ipa_char("d")
-        + "]!")
+    res = ''
+    i = 0
+    while i < len(str):
+        if str[i:i+4] in tokens:
+            res += data[str[i:i+4]]["ipa"]
+            i += 4
+        elif str[i:i+3] in tokens:
+            res += data[str[i:i+3]]["ipa"]
+            i += 3
+        elif str[i:i+2] in tokens:
+            res += data[str[i:i+2]]["ipa"]
+            i += 2
+        elif str[i] in tokens:
+            res += data[str[i]]["ipa"]
+            i += 1
+        else:
+            res += ' '
+            i += 1
     
-hello_world_by_chars()
+    return res
+
+def get_diacritic_by_code(code):
+    for key, val in data.items():
+        # print(key, val)
+        if isinstance(val, dict) and val.get("code") != None and val.get("code") == code:
+            return key
+    return ''
+
+print("[" + parse_str("<F>\"hE%5o <R>w@`ld") + "]!")
